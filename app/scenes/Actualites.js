15b940ed568e488df694e785db5a119b8257913d
jinnlao <daniel.guggenheim@epfl.ch>
2017-03-08 17:13:32 +0100

    Actualites code improvements

--- app/scenes/Actualites.js
+++ app/scenes/Actualites.js
@@ -1,26 +1,26 @@
+'use strict';
 
 import React, { Component, PropTypes } from 'react';
+import { Image, Platform, RefreshControl, StyleSheet, } from 'react-native';
 import {
-  Platform,
-  AppRegistry,
-  StyleSheet,
-  Image,
-  RefreshControl
-} from 'react-native';
-import { Footer, FooterTab, Spinner, Text, View, Content, Container, Header, Title, Button, Icon, ListItem, List } from 'native-base';
-import myTheme from '../themes/myTheme';
-// import * as firebase from 'firebase';
+  Spinner, Text, View, Container, Header, Title, Button,
+  List, ListItem,
+} from 'native-base';
 
 import moment from 'moment';
 import 'moment/locale/fr';
 
+import myTheme from '../themes/myTheme';
+
 var GLOBAL = require('../global/GlobalVariables');
 
 const NB_OF_LAST_DAYS_WITH_HUMANIZED_DATE = 7;
+const INTRO_MAX_NB_OF_LINE = 3;
 var logo_icon = require("../images/logo/logo.png");
 
-
 const propTypes = {
+  // Articles data loaded from the backend
+  articlesContent: React.PropTypes.arrayOf(PropTypes.string),
   articlesInfo: React.PropTypes.arrayOf(
     React.PropTypes.shape({
       category: PropTypes.string,
@@ -30,12 +30,12 @@ const propTypes = {
       title: PropTypes.string,
     }
     )),
-  articlesContent: React.PropTypes.arrayOf(PropTypes.string),
-  fetchBackendToUpdateAll: React.PropTypes.func.isRequired,
-  loading: React.PropTypes.bool.isRequired,
-  goToActualitesDetails: React.PropTypes.func.isRequired,
-};
 
+  // Other props
+  fetchBackendToUpdateAll: React.PropTypes.func.isRequired,
+  goToActualitesDetails: React.PropTypes.func.isRequired,
+  loading: React.PropTypes.bool.isRequired,
+};
 
 const defaultProps = {
   articlesInfo: [],
@@ -43,29 +43,159 @@ const defaultProps = {
 };
 
 
+
+/**
+ * Scene containing the list of articles summary. Each article summary can be clicked on
+ * which will lead to a page with the complete article.
+ * 
+ * It will use 2 main props: articlesContent and articlesInfo.
+ * - articlesInfo will contain all the useful information about an article that will be shown
+ * in this view. (such as title, category, creation date, etc..)
+ * - articlesContent will contain the html code of the article. It will not be shown here but will
+ * be transfered to the detailed view of the article is the user click on it.
+ */
 class Actualites extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-       refreshing: false,
+      refreshing: false,
     }
     moment.locale('fr');
   }
-  _onRefresh() {
-    this.setState({refreshing: true});
-    this.props.fetchBackendToUpdateAll()
-    .then(() => {
-      this.setState({refreshing: false});
-    })
-    .catch((error) => {
-      console.log('Error while refreshing data in articles actualites.');
-      this.setState({refreshing: false});
-    });
-  }
-  getFormatedDate(strDate) {
-    let momentDate = moment(strDate, "YYYY-MM-DD");
-    let diffInDays = moment().diff(momentDate, 'days');
 
+  render() {
+    let articles = this.props.articlesInfo;
+    let articlesHTML = this.props.articlesContent;
+
+    return (
+      <Container theme={myTheme}>
+
+        <Header>
+          <Button transparent disabled>
+            <Image resizeMode={"contain"} style={{ width: 32 }} source={logo_icon} />
+          </Button>
+          <Title>Actualités</Title>
+        </Header>
+
+        {/* --- Main view: renders the loading spin or the article. --- */}
+        <View>
+          {this.props.loading ?
+            <View style={styles.spinnerView}><Spinner /></View>
+            :
+            this._renderArticlesList()
+          }
+        </View>
+
+      </Container>
+    );
+  }
+
+
+  /**
+   * Renders the article list, or if the articles have not loaded yet, renders a temporary view. 
+   */
+  _renderArticlesList() {
+    let articles = this.props.articlesInfo;
+    let articlesHTML = this.props.articlesContent;
+
+    //Test if the articles have been loaded yet
+    if (typeof articles != "undefined" && articles != null && articles.length > 0 &&
+      typeof articlesHTML != "undefined" && articlesHTML != null && articlesHTML.length > 0 &&
+      articles.length == articlesHTML.length) {
+
+      // If it is the case, return the article list
+      return (
+        <List dataArray={articles}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh.bind(this)}
+            />
+          }
+          renderRow={(article) =>
+            <ListItem
+              button
+              onPress={() => this.props.goToActualitesDetails(article, articlesHTML[article.id])}
+            >
+              <View>
+                <View style={styles.categoryAndDate}>
+                  <View style={styles.categoryWithSquare}>
+                    <Text style={styles.category}>
+                      {article.category}
+                    </Text>
+                  </View>
+                  <Text style={styles.date}>
+                    {this._getFormatedDate(article.date)}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={styles.titreArticle}>
+                    {article.title}
+                  </Text>
+                  <Text style={styles.introArticle} numberOfLines={INTRO_MAX_NB_OF_LINE}>
+                    {article.intro}
+                  </Text>
+                </View>
+              </View>
+            </ListItem>
+          } />
+      );
+
+    }
+    /*
+    If the articles have not been loaded yet, renders a message and a reload button.
+    (even if reloading is automatically done in background)
+    */
+    else {
+      return (
+        <View>
+          <Text style={styles.emptyArticlesText}>
+            Connectez-vous à Internet pour télécharger les derniers articles.
+          </Text>
+          <Button
+            success
+            onPress={() => this.props.fetchBackendToUpdateAll()}
+            style={styles.emptyArticlesButton}>
+            Rafraîchir la page
+          </Button>
+        </View>
+      );
+    }
+  }
+
+
+  /**************************  USEFUL METHODS  **************************/
+
+  /**
+  * Activates the refresh of the page. It will load the procedure to update all the article list.
+  */
+  _onRefresh() {
+    this.setState({ refreshing: true });
+    this.props.fetchBackendToUpdateAll()
+      .then(() => {
+        this.setState({ refreshing: false });
+      })
+      .catch((error) => {
+        console.log('Error while refreshing data in articles actualites.');
+        this.setState({ refreshing: false });
+      });
+  }
+
+
+  /**
+   * Parse and transform a date in a nice format to read.
+   * @param {String} strDate : A string in a date format (YYYY-MM-DD).
+   * @return {String} : The date formated to be visually nice
+   */
+  _getFormatedDate(strDate) {
+    let momentDate = moment(strDate, "YYYY-MM-DD", true);
+    if (!momentDate.isValid()) {
+      return "Date indisponible";
+    }
+
+    // For the last week, get a nicer format. 
+    let diffInDays = moment().diff(momentDate, 'days');
     if (diffInDays == 0) {
       return "Aujourd'hui";
     } else if (diffInDays == 1) {
@@ -78,88 +208,10 @@ class Actualites extends Component {
       return momentDate.format("D MMM YYYY");
     }
   }
-
-
-
-  render() {
-    let articles = this.props.articlesInfo;
-    let articlesHTML = this.props.articlesContent;
-
-    return (
-      <Container theme={myTheme}>
-        <Header>
-          <Button transparent disabled>
-            <Image resizeMode={"contain"} style={{ width: 32 }} source={logo_icon} />
-          </Button>
-          <Title>Actualités</Title>
-          {/*<Button transparent onPress={() => this.props.fetchBackendToUpdateAll()}>
-            <Icon name='ios-refresh' />
-          </Button>*/}
-        </Header>
-
-        <View>
-          {this.props.loading ?
-            <View style={styles.spinnerView}><Spinner /></View> :
-            this._renderArticlesList()}
-        </View>
-      </Container>
-    );
-  }
-
-
-
-  _renderArticlesList() {
-    let articles = this.props.articlesInfo;
-    let articlesHTML = this.props.articlesContent;
-
-    //Test if the array is defined
-    if (typeof articles != "undefined" && articles != null && articles.length > 0 &&
-      typeof articlesHTML != "undefined" && articlesHTML != null && articlesHTML.length > 0 &&
-      articles.length == articlesHTML.length) {
-      // Return list of articles
-      return (
-        <List dataArray={articles}
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this._onRefresh.bind(this)}
-            />
-          }
-          renderRow={(article) =>
-            <ListItem button onPress={() => this.props.goToActualitesDetails(article, articlesHTML[article.id])}>
-              <View>
-                <View style={styles.categoryAndDate}>
-                  <View style={styles.categoryWithSquare}>
-                    {/*<Icon name='ios-square' style={[styles.categorySquare, { color: 'blue' }]} />*/}
-                    <Text style={styles.category}>{article.category}</Text>
-                  </View>
-                  <Text style={styles.date}>{this.getFormatedDate(article.date)}</Text>
-                </View>
-                <View>
-                  <Text style={styles.titreArticle}>{article.title}</Text>
-                  <Text style={styles.introArticle} numberOfLines={3}>{article.intro}</Text>
-                </View>
-              </View>
-            </ListItem>
-          } />
-      );
-
-    } else {
-      //A page with an empty list of articles.
-      return (
-        <View style={styles.emptyArticlesView}>
-          <Text style={styles.emptyArticlesText}>Connectez-vous à Internet pour télécharger les derniers articles.</Text>
-          <Button
-            success
-            onPress={() => this.props.fetchBackendToUpdateAll()}
-            style={styles.emptyArticlesButton}>
-            Rafraîchir la page
-          </Button>
-        </View>
-      );
-    }
-  }
 }
+
+
+/**************************  STYLESHEET  **************************/
 
 const styles = StyleSheet.create({
   spinnerView: {
@@ -173,19 +225,15 @@ const styles = StyleSheet.create({
   categoryWithSquare: {
     flexDirection: 'row',
     alignItems: 'center',
-    // backgroundColor:'yellow',
   },
   categorySquare: {
-    // color:'red',
-    fontSize: 10,
     marginRight: 5,
     marginTop: 5,
+    fontSize: 10,
   },
   category: {
-    // fontStyle: 'italic',
     color: '#5A5D5E',
-    fontSize:  (Platform.OS === 'ios') ? 13 : 14,
-    // backgroundColor:'orange',
+    fontSize: (Platform.OS === 'ios') ? 13 : 14,
   },
   date: {
     fontSize: (Platform.OS === 'ios') ? 13 : 14,
@@ -199,18 +247,6 @@ const styles = StyleSheet.create({
     marginTop: (Platform.OS === 'ios') ? 2 : 0,
     marginBottom: (Platform.OS === 'ios') ? 5 : 0,
   },
-
-
-  //Empty articles view:
-  emptyArticlesView: {
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // flex: 1,
-    // backgroundColor: 'orange',
-    // textAlign:'center',
-    // flexDirection: 'row',
-
-  },
   emptyArticlesText: {
     marginTop: 8,
     textAlign: 'center',
@@ -220,8 +256,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
 });
-
-
 
 Actualites.propTypes = propTypes;
 Actualites.defaultProps = defaultProps;
